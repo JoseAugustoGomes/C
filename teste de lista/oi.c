0e7242f0fd11905642541f57a7b75c7b3660b25f
josé augusto gomes <165060723+JoseAugustoGomes@users.noreply.github.com>
2024-04-05 16:56:16 -0300

    adicionar o tipo lista
    - adicionado o metodo de lista
    - adicionado metodo de printar elementos da lista

--- teste de lista/oi.c
+++ teste de lista/oi.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main (){
+struct no{
+    int valor;
+    struct no *proximo;
+};
 
-    printf("oioi\n");
+typedef struct no No;
+typedef struct {
+    No *inicio;
+}lista;
+
+void imprimir(lista *l){
+    No *auxiliar;
+    auxiliar = l->inicio;
+
+    if(auxiliar == NULL){
+        printf("lista vazia\n");
+    }else {
+        while (auxiliar != NULL){
+            printf("%i ", auxiliar->valor);
+            auxiliar = auxiliar->proximo;
+        }
+    printf("\n");
+    }
+}
+
+int main() {
+    No *no1, *no2;
+    lista l;
+
+    no1 = malloc(sizeof(No));
+    no2 = malloc(sizeof(No));
+
+    no1->valor = 5;
+    no2->valor = 10;
+    no1->proximo = no2;
+    no2-> proximo = NULL;
+    l.inicio = no1;
+    imprimir(&l);
+
     return 0;
 }
